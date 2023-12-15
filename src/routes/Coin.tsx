12e4c5406da2936712ecf5761fa99429f4b149e9
Danbi <silverwill39@gmail.com>
2023-12-16 01:24:11 +0900

    5.4 Route States

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,12 +1,51 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.div`
+  text-align: center;
+  display: block;
+`;
 
 interface RouteParams {
   coinId: string;
 }
 
+interface routeState {
+  name: string;
+}
+
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>(); // useParams는 URl의 파라미터들을 반환하는 함수
   // const { coinId } = useParams<coinId:string>();  // 이렇게 해도 됨
-  return <h1>COIN : {coinId}</h1>;
+  const { state } = useLocation<routeState>();
+
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
 export default Coin;
