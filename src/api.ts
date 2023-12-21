76130bac6e7048cca5970413b9ca8046cce94332
Danbi <silverwill39@gmail.com>
2023-12-21 21:13:48 +0900

    5.13 Price Chart part  2

--- src/api.ts
+++ src/api.ts
@@ -13,11 +13,11 @@ export async function fetchCoinTickers(coinId: string) {
 }
 
 export async function fetchCoinHistory(coinId: string) {
-  const endDate = Math.floor(Date.now() / 1000); // 현재
-  const startDate = endDate - 60 * 60 * 24 * 7 * 2; // 현재으로부터 이주일 전
   return await (
     await fetch(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
   )
+    // const endDate = Math.floor(Date.now() / 1000); // 현재
+    // const startDate = endDate - 60 * 60 * 24 * 7 * 2; // 현재으로부터 이주일 전
     // await fetch(`${BASE_URL}/tickers/${coinId}/ohlcv/historcal?start=${startDate}&end={endDate}`) coinpaprika의 유료화로 인해 데이터를 가져올 수 없음
     .json();
 }
--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,13 +1,69 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexCharts from "react-apexcharts";
+
+interface IData {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
 
 interface chartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: chartProps) {
-  const {} = useQuery(["ohlcv", coinId], () => fetchCoinHistory(coinId));
-  return <h1>Chart</h1>;
+  const { isLoading, data } = useQuery<IData[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexCharts
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => parseFloat(price.close)) ?? [],
+              // ??: null 병합 연산자 (앞의 값이 null 또는 undefined일 때 [](뒤의 값) 반환)
+              // close 데이터가 string이기 때문에 parseFloat를 통해 형 변환을 시켜줘야 함
+            },
+          ]}
+          options={{
+            chart: {
+              height: 500,
+              width: 300,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            theme: { mode: "dark" },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              labels: { show: false },
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Chart;
