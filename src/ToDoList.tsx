761909f7fa2279207c05cd32b9834c46fc4723ee
Danbi <silverwill39@gmail.com>
2023-12-22 15:58:49 +0900

    6.6 Forms

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
   const [toDo, setToDo] = useState("");
   const [toDoError, setToDoError] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
@@ -24,6 +25,26 @@ function ToDoList() {
         <input onChange={onChange} value={toDo} placeholder="Write a to do!" />
         <button>Add</button>
         {toDoError !== "" ? toDoError : null}
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  // register함수는 onBlur 이벤트, onChange 이벤트, ref, name을 가짐
+  // watch함수는 form의 입력값들의 변화를 관찰
+  console.log(watch());
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("userName")} placeholder="User Name" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password2")} placeholder="Password2" />
+        <button>Add</button>
       </form>
     </div>
   );
