6a3ba9622de59c9f2c515f2e4f348bdf48a7a498
박단비 <silverwill39@gmail.com>
2023-12-27 16:54:01 +0900

    6.9 Custom Validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -37,6 +37,7 @@ interface IForm {
   userName: string;
   password: string;
   password2: string;
+  extraError?: string;
 }
 
 function ToDoList() {
@@ -44,18 +45,28 @@ function ToDoList() {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password2) {
+      setError(
+        "password2",
+        { message: "Password are not the same" },
+        { shouldFocus: true } // shouldFocus는 에러가 발생하면 커서를 해당 input에 focus시켜줌
+      );
+    }
+    // setError("extraError", { message: "Server offline" });
   };
+  console.log(errors);
   // register함수는 onBlur 이벤트, onChange 이벤트, ref, name을 가짐
   // watch함수는 form의 입력값들의 변화를 관찰함
   // handleSubmit은 validation을 담당함
   // formState.errors는 에러를 발생시킴
+  // setError는 발생하는 문제에 따라 추가적으로 에러를 설정할 수 있게 도와줌
 
   return (
     <div>
@@ -80,7 +91,16 @@ function ToDoList() {
         />
         <span>{errors?.firstName?.message as ""}</span>
         <input
-          {...register("lastName", { required: "write here" })}
+          {...register("lastName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value?.includes("nico") ? "No nicos allowed" : true,
+              // validate: (nico를 포함하지 않는다면 true 반환) validate를 통과하지 못해서 발생하는 에러
+              noNick: (value) =>
+                value?.includes("nick") ? "No nick allowed" : true,
+            },
+          })}
           placeholder="Last Name"
         />
         <span>{errors?.lastName?.message as ""}</span>
@@ -112,6 +132,7 @@ function ToDoList() {
         />
         <span>{errors?.password2?.message as ""}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message as ""}</span>
       </form>
     </div>
   );
